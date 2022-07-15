f44889eaa5de1868be74748790f2e59629587c68
Taylor Kearns <taylor@thegnar.co>
2022-07-15 16:43:44 -0400

    Fix rendering wonk by using partial everywhere
    https://web-crunch.com/posts/digging-into-turbo-with-ruby-on-rails-7

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,7 +3,7 @@ class AlbumsController < ApplicationController
 
   # GET /albums or /albums.json
   def index
-    @albums = Album.all
+    @albums = Album.all.reverse
   end
 
   # GET /albums/1 or /albums/1.json
